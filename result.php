c88983d58f048ed06db030dbb7c47f27fdd33c2a
willow-12 <yosefayalew46@gmail.com>
2026-04-24 09:40:46 +0300

    Refactor result.php to simplify output structure

--- result.php
+++ result.php
@@ -2,21 +2,13 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Result</title>
-</head>
-<body>
-
 <h2>Registration Successful 🎉</h2>
+
+<p>Welcome <?php echo $_SESSION['first_name']; ?> 👋</p>
 
 <p>First Name: <?php echo $_SESSION['first_name']; ?></p>
 <p>Last Name: <?php echo $_SESSION['last_name']; ?></p>
 <p>Department: <?php echo $_SESSION['department']; ?></p>
 <p>Gender: <?php echo $_SESSION['gender']; ?></p>
-<p>Hobbies: <?php echo implode(", ", $_SESSION['hobbies']); ?></p>
+<p>Hobbies: <?php echo $_SESSION['hobbies']; ?></p>
 <p>Others: <?php echo $_SESSION['others']; ?></p>
-
-</body>
-</html>
